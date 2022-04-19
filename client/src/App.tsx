0cb142d236d3a28b83969229e0cf0fd93eea268c
talhaabbasi <talhaabbasi1997@gmail.com>
2022-04-19 07:36:15 +0500

    [FEAT] Add Navbar :muscle:

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,8 +8,14 @@ import {
 } from "./components"
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   )
 }
