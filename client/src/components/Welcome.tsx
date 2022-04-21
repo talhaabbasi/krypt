dda20c4217a52d52e0c51186b8a11f9bab9f8e35
talhaabbasi <talhaabbasi1997@gmail.com>
2022-04-22 00:55:01 +0500

    [FEAT] Add Welcome section

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,5 +1,22 @@
+import { AiFillPlayCircle } from "react-icons/ai"
+import { SiEthereum } from "react-icons/si"
+import { BsInfoCircle } from "react-icons/bs"
 const Welcome = () => {
-  return <h1>Welcome</h1>
+  return (
+    <div className="flex w-full justify-center items-center">
+      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col md:mr-10">
+          <h1 className="text-3xl sm:ext-5xl text-white text-gradient py-1">
+            Send Crypto <br />
+            across the world
+          </h1>
+          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+            Buy and sell cryptocurrencies easily on Krypto
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Welcome
