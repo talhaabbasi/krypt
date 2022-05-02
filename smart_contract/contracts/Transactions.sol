db86fd2085e77c62cfd829fe1afd92db3130675d
talhaabbasi <talhaabbasi1997@gmail.com>
2022-05-03 02:28:37 +0500

    [FEAT] Add smart contract

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,66 @@
+// PDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Transactions {
+    uint256 transactionsCount;
+
+    event Transfer(
+        address from,
+        address reciever,
+        uint256 amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
+
+    struct TransferStruct {
+        address sender;
+        address reciever;
+        uint256 amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBlockchain(
+        address payable reciever,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public {
+        transactionsCount += 1;
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                reciever,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
+
+        emit Transfer(
+            msg.sender,
+            reciever,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
+    }
+
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
+        return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionsCount;
+    }
+}
